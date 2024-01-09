cf997fed7ad6266d7efad9ddfd24767dc6d6cb05
TugceYavasoglu <y.tugce19@gmail.com>
2024-01-09 21:27:39 +0300

    Add project files.

---
+++ GameProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GameProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.Add(new Gamer
+            {
+                Id = 1,
+                BirthYear = 2000,
+                FirstName = "Tuğçe",
+                LastName = "Yavaşoğlu",
+                IdentityNumber = 12345
+            });
+        }
+    }
+}
